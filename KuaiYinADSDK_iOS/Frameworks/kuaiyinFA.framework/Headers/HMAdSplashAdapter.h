7bc8f7ab29861ca908ab7e0ac41eaafdf5e44360
LeiZhengFa <12456715@qq.com>
2021-10-09 18:04:11 +0800

    2155

---
+++ KuaiYinADSDK_iOS/Frameworks/kuaiyinFA.framework/Headers/HMAdSplashAdapter.h
@@ -0,0 +1,22 @@
+//
+//  HMAdSplashAdapter.h
+//  KuaiYinADProject
+//
+//  Created by 法正磊 on 2021/10/8.
+//
+
+#import <UIKit/UIKit.h>
+#import "HMAdSplashDelegate.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol HMAdSplashAdapter <NSObject>
+
+/// 数据模型
+@property (nonatomic, strong) HMAdSplashModel *splashModel;
+
+/// 版本号
++ (NSString *)adVersion;
+
+@end
+
+NS_ASSUME_NONNULL_END
