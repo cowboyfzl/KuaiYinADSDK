399674f33ab2f7576eb58b15717c4d0497ff3ed0
LeiZhengFa <12456715@qq.com>
2021-10-09 17:25:18 +0800

    12314

---
+++ KuaiYinADSDK_iOS/Frameworks/kuaiyinFA.framework/Headers/kuaiyinFA.h
@@ -0,0 +1,18 @@
+//
+//  kuaiyinFA.h
+//  kuaiyinFA
+//
+//  Created by 法正磊 on 2021/10/9.
+//
+
+#import "HMAdSplashManager.h"
+#import "HMAdTTSplashRegister.h"
+//! Project version number for kuaiyinFA.
+FOUNDATION_EXPORT double kuaiyinFAVersionNumber;
+
+//! Project version string for kuaiyinFA.
+FOUNDATION_EXPORT const unsigned char kuaiyinFAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kuaiyinFA/PublicHeader.h>
+
+
