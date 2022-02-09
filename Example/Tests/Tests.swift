cc7e9f98376394aaa75f05e4e7f9998f344de2d3
LeiZhengFa <12456715@qq.com>
2022-02-09 17:51:46 +0800

    拉克丝的晶澳科技

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -8,6 +8,7 @@ class Tests: XCTestCase {
         let a = "asdadas "
         "阿萨德明看来你达克赛德晶澳科技颠三倒四拿到了你"
         "卡死了对话框拉胯还是的垃圾"
+        let c = "拉克丝的就拉胯"
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
