297e894c43d33e1902b10c67741714f060f4d741
LeiZhengFa <12456715@qq.com>
2022-02-09 17:13:53 +0800

    调整A

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -5,6 +5,7 @@ class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        "alishdalksdhkajhdal"
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
