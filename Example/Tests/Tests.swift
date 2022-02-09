7f9958e0189fe6a46b19c2fee76098335f37b5c5
LeiZhengFa <12456715@qq.com>
2022-02-09 17:28:07 +0800

    调整啊

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -5,6 +5,7 @@ class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        "脸卡手机打开拉家带口垃圾的"
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
